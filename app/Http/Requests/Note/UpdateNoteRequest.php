039e3d39e96f9a0d79c8be8d5397b7dd137b2cd9
Hank Tsou <28382639+Hank-Tsou@users.noreply.github.com>
2025-06-14 22:40:02 -0700

    feature/request

---
+++ app/Http/Requests/Note/UpdateNoteRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Note;
+
+use App\Enums\NoteStatus;
+use App\Enums\NoteType;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateNoteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'body' => ['required', 'string'],
+            'type' => ['sometimes', Rule::in(array_column(NoteType::cases(), 'value'))],
+            'status' => ['sometimes', Rule::in(array_column(NoteStatus::cases(), 'value'))],
+            'assigned_to' => ['nullable', 'exists:users,id'],
+        ];
+    }
+}
