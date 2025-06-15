4a74adb8847987976824700e4b37ef0fb61f05df
Hank Tsou <28382639+Hank-Tsou@users.noreply.github.com>
2025-06-14 22:34:03 -0700

    feature/models

---
+++ app/Models/Merchant.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merchant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uid',
+        'name',
+        'phone',
+        'email',
+        'two_factor_enabled',
+        'category',
+    ];
+
+//    protected $with = ['notes'];
+
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
+}
---
+++ app/Models/Note.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uid',
+        'title',
+        'body',
+        'type',
+        'status',
+        'merchant_id',
+        'created_by',
+        'assigned_to',
+    ];
+
+    public function merchant()
+    {
+        return $this->belongsTo(Merchant::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
