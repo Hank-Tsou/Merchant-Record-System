e7129716a678c399cbad609b436a2309a4fc9bb8
Hank Tsou <28382639+Hank-Tsou@users.noreply.github.com>
2025-06-14 22:38:19 -0700

    feature/route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MerchantController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +13,13 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('merchant', [MerchantController::class, 'index'])->name('profile');
+    Route::get('/merchants/{merchant}/notes', [NoteController::class, 'getMerchantNotes'])->name('getMerchantNotes');
+    Route::apiResource('notes', NoteController::class);
+});
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
